79d710048b3f85fcdb945efe753b7c658a9e3dc1
GuilhermeSK2 <iosfantasma08@gmail.com>
2024-06-29 13:14:49 -0300

    AprendizadoTeste

---
+++ TokDCereja-back-end/Migrations/20240629152157_AprendizadoMigration01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TokDCereja_back_end.Migrations
+{
+    /// <inheritdoc />
+    public partial class AprendizadoMigration01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aprendizados",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Conteudo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataPublicacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aprendizados", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aprendizados");
+        }
+    }
+}
